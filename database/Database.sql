33d813bd970119bef716a4863b199b653ac4a6d1
Anh Tu <tuhase161714@fpt.edu.vn>
2022-05-21 00:12:24 +0700

    Update Database.sql

--- database/Database.sql
+++ database/Database.sql
@@ -5,14 +5,7 @@ CREATE DATABASE SWP391
 -- SELECT * FROM Semester
 -- SELECT * FROM Business
 -- SELECT * FROM Category
-
-CREATE TABLE Student (
-    StudentId INT PRIMARY KEY,
-    Name VARCHAR(30),
-    Password VARCHAR(30) NOT NULL,
-    Status INT,
-    Email VARCHAR(50) UNIQUE
-)
+USE  SWP391
 
 CREATE TABLE Lecturer (
     LecturerId INT PRIMARY KEY,
@@ -20,13 +13,6 @@ CREATE TABLE Lecturer (
     Password VARCHAR(30) NOT NULL,
     Status INT,
     Email VARCHAR(50) UNIQUE
-)
-
-CREATE TABLE Semester (
-    SemesterId INT PRIMARY KEY,
-    Name VARCHAR(30),
-    StartDate DATE,
-    EndDate DATE
 )
 
 CREATE TABLE Business (
@@ -39,4 +25,68 @@ CREATE TABLE Business (
 CREATE TABLE Category (
     CategoryId INT PRIMARY KEY,
     CategoryName VARCHAR(30),
+)
+
+CREATE TABLE Topic(
+	TopicId INT PRIMARY KEY,
+	Name VARCHAR(30),
+	CategoryId INT FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId),
+	Description VARCHAR(200),
+	BusinessId INT FOREIGN KEY (BusinessId) REFERENCES Business(BusinessId),
+	LecturerId INT FOREIGN KEY (LecturerId) REFERENCES Lecturer(LecturerId)
+)
+
+CREATE TABLE Semester (
+    SemesterId INT PRIMARY KEY,
+    Name VARCHAR(30),
+    StartDate DATE,
+    EndDate DATE,
+	TopicId INT FOREIGN KEY (TopicId) REFERENCES Topic(TopicId)
+)
+
+ CREATE TABLE Department (
+	DepartmentId INT PRIMARY KEY,
+	Name VARCHAR(30)
+ )
+
+ CREATE TABLE DepartmentTopic (
+	Id INT PRIMARY KEY,
+	DepartmentId INT NOT NULL FOREIGN KEY  (DepartmentId) REFERENCES Department(DepartmentId),
+	TopicId INT NOT NULL FOREIGN KEY  (TopicId) REFERENCES Topic(TopicId)
+ )
+
+ CREATE TABLE Student (
+    StudentId INT PRIMARY KEY,
+    Name VARCHAR(30),
+    Password VARCHAR(30) NOT NULL,
+    Status INT,
+    Email VARCHAR(50) UNIQUE,
+	DepartmentId INT FOREIGN KEY (DepartmentId) REFERENCES Department(DepartmentId)
+)
+
+CREATE TABLE Groups (
+	GroupId INT PRIMARY KEY,
+	GroupName VARCHAR(30),
+)
+
+CREATE TABLE StudentGroup (
+	Id INT PRIMARY KEY,
+	StudentId INT NOT NULL FOREIGN KEY (StudentId) REFERENCES Student(StudentId),
+	GroupId INT NOT NULL FOREIGN KEY (GroupId) REFERENCES Groups(GroupId)
+)
+
+CREATE TABLE Project(
+	ProjectId INT PRIMARY KEY,
+	Description VARCHAR(200),
+	Name VARCHAR(30),
+	SourceCode VARCHAR(200),
+	TopicId INT FOREIGN KEY(TopicId) REFERENCES Topic(TopicId),
+	Status INT,
+	GroupId INT UNIQUE NOT NULL FOREIGN KEY(GroupId) REFERENCES Groups(GroupId)
+)
+
+CREATE TABLE ProjectLecturer(
+	Id INT PRIMARY KEY,
+	ProjectId INT NOT NULL FOREIGN KEY (ProjectId) REFERENCES Project(ProjectId),
+	LecturerId INT NOT NULL FOREIGN KEY (LecturerId) REFERENCES Lecturer(LecturerId)
 )
