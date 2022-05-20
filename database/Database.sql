5f2eb997755050cfcf9dea36b6857ef925ed349b
Quang <hongquang7302@gmail.com>
2022-05-20 21:33:37 +0700

    database

--- database/Database.sql
+++ database/Database.sql
@@ -1,10 +1,42 @@
 CREATE DATABASE SWP391
 -- DROP DATABASE SWP391
+-- SELECT * FROM Student
+-- SELECT * FROM Lecturer
+-- SELECT * FROM Semester
+-- SELECT * FROM Business
+-- SELECT * FROM Category
 
 CREATE TABLE Student (
     StudentId INT PRIMARY KEY,
-    Password VARCHAR(30) NOT NULL,
     Name VARCHAR(30),
+    Password VARCHAR(30) NOT NULL,
     Status INT,
     Email VARCHAR(50) UNIQUE
+)
+
+CREATE TABLE Lecturer (
+    LecturerId INT PRIMARY KEY,
+    Name VARCHAR(30),
+    Password VARCHAR(30) NOT NULL,
+    Status INT,
+    Email VARCHAR(50) UNIQUE
+)
+
+CREATE TABLE Semester (
+    SemesterId INT PRIMARY KEY,
+    Name VARCHAR(30),
+    StartDate DATE,
+    EndDate DATE
+)
+
+CREATE TABLE Business (
+    BusinessId INT PRIMARY KEY,
+    Name VARCHAR(30),
+    Password VARCHAR(30) NOT NULL,
+    Email VARCHAR(50) UNIQUE
+)
+
+CREATE TABLE Category (
+    CategoryId INT PRIMARY KEY,
+    CategoryName VARCHAR(30),
 )
