e31851311836dc20b574f66aa5aae40fbd17ae98
Quang <hongquang7302@gmail.com>
2022-06-05 00:29:25 +0700

    pull ve nhe

--- swp391/src/java/DAO/TestConnection.java
+++ swp391/src/java/DAO/TestConnection.java
@@ -5,6 +5,7 @@
  */
 package DAO;
 
+import DTO.Groups;
 import DTO.Semester;
 import DTO.Student;
 import DTO.StudentGroup;
@@ -30,30 +31,24 @@ public class TestConnection {
 //        for (Student student : list) {
 //            System.out.println(student.getStudentId()+","+student.getName()+","+student.getPassword()+","+student.getStatus()+","+student.getEmail()+","+student.getDepartmentId());
 //        } 
-
 //******************************************
 //        test student create()
-
 //        Student student = new Student(6,"Ha Anh Tu31","12345678123",1,"haanhtu@g123mail.com",2);
 //        StudentDAO.create(student);
 //        System.out.println(student.getStudentId()+","+student.getName()+","+student.getPassword()+","+student.getStatus()+","+student.getEmail()+","+student.getDepartmentId());
 //******************************************
 //        test student read()
-
 //        Student student = StudentDAO.read(3);
 //        System.out.println(student.getStudentId()+","+student.getName()+","+student.getPassword()+","+student.getStatus()+","+student.getEmail()+","+student.getDepartmentId());
 //******************************************
 //        test student update()
-
-//        Student student = new Student(3,"Ha Anh Tu31","12345678123",1,"haanhtu@g123mail.com",2);
+//        Student student = new Student(3,"Ha Anh Tu1","12345678123",0,"haanhtu@g123mail.com",2);
 //        StudentDAO.update(student);
 //******************************************
 //        test student delete()
-
 //        StudentDAO.delete(6);
 //******************************************
 //        test studentGroup readAll()
-
 //        ArrayList<StudentGroup> list = StudentGroupDAO.readAll();
 //
 //        for (StudentGroup student : list) {
@@ -61,12 +56,10 @@ public class TestConnection {
 //        }
 //******************************************
 //        test studentGroup read()
-
 //        StudentGroup student = StudentGroupDAO.read(3);
 //        System.out.println(student.getId() + "," + student.getStudentId()+ "," + student.getGroupId()+ "," + student.getLeaderStatus());
 //******************************************
 //        test studentGroup create()
-
 //          Student student6 = new Student(6,"Nguyen Van A","12345678123",1,"nguyenvana@g123mail.com",2);
 //          StudentDAO.create(student6);
 //          StudentGroup studentGroup = new StudentGroup(6,6,1,0);
@@ -76,16 +69,12 @@ public class TestConnection {
 //          for (StudentGroup student : list) {
 //            System.out.println(student.getId() + "," + student.getStudentId()+ "," + student.getGroupId()+ "," + student.getLeaderStatus());
 //          }
-        
 //******************************************
 //          test studentGroup update()
-
 //          StudentGroup studentGroup = new StudentGroup(3,5,1,0);
 //          StudentGroupDAO.update(studentGroup);
-
 //******************************************
 //          test studentGroup delete()
-
 //          StudentGroupDAO.delete(6);
 //          ArrayList<StudentGroup> list = StudentGroupDAO.readAll();
 //
@@ -94,7 +83,6 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Topic readAll()
-
 //          ArrayList<Topic> list = TopicDAO.readAll();
 //
 //          for (Topic topic : list) {
@@ -102,12 +90,10 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Topic read()
-
 //          Topic topic = TopicDAO.read(2);
 //          System.out.println(topic.getTopicId()+ "," + topic.getName()+ "," + topic.getCatergoryId()+ "," + topic.getDescription()+ "," + topic.getBusinessId()+ "," + topic.getDepartmentId());
 //******************************************
 //          test Topic create()
-
 //          Topic topic1 = new Topic(5,"design",10,"beautyfull design",5,2);
 //          TopicDAO.create(topic1);
 //          ArrayList<Topic> list = TopicDAO.readAll();
@@ -117,7 +103,6 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Topic update()
-
 //          Topic topic1 = new Topic(5,"Testing",10,"beautyfull design",5,2);
 //          TopicDAO.update(topic1);
 //          ArrayList<Topic> list = TopicDAO.readAll();
@@ -127,7 +112,6 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Topic delete()
-
 //          TopicDAO.delete(5);
 //          ArrayList<Topic> list = TopicDAO.readAll();
 //          for (Topic topic : list) {
@@ -135,7 +119,6 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Semester readAll()
-
 //          ArrayList<Semester> list = SemesterDAO.readAll();
 //
 //          for (Semester sem : list) {
@@ -143,12 +126,10 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Semester read()
-
 //          Semester sem = SemesterDAO.read(2);
 //          System.out.println(sem.getSemesterId()+ "," + sem.getName()+ "," + sem.getStartDate()+ "," + sem.getEndDate()+ "," + sem.getTopicId());
 //******************************************
 //          test Semester create()
-
 //          SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 //          Date startDate=null;
 //          Date endDate=null;
@@ -166,7 +147,6 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Semester update()
-
 //          SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 //          Date startDate=null;
 //          Date endDate=null;
@@ -184,11 +164,33 @@ public class TestConnection {
 //          }
 //******************************************
 //          test Semester delete()
-
 //          SemesterDAO.delete(5);
 //          ArrayList<Semester> list = SemesterDAO.readAll();
 //          for (Semester sem : list) {
 //            System.out.println(sem.getSemesterId()+ "," + sem.getName()+ "," + sem.getStartDate()+ "," + sem.getEndDate()+ "," + sem.getTopicId());
 //          }
+//*******************************************
+//           test Groups readAll()
+//           ArrayList<Groups> list = GroupsDAO.readAll();
+//           for (Groups group : list) {
+//              System.out.println(group.getGroupId() + "," + group.getGroupName()+ "," + group.getSemId());
+//           }
+//******************************************
+//        test Groups create()
+//        Groups groups = new Groups(5,"Group 5",4);
+//        GroupsDAO.create(groups);
+//        System.out.println(groups.getGroupId()+","+groups.getGroupName()+","+groups.getSemId());
+//******************************************
+//        test Groups read()
+//        Groups groups = GroupsDAO.read(3);
+//        System.out.println(groups.getGroupId()+","+groups.getGroupName()+","+groups.getSemId());
+//******************************************
+//        test Groups update()
+//        Groups groups = new Groups(2,"Worst group",2);
+//        GroupsDAO.update(groups);
+//******************************************
+//        test Groups delete()
+//        GroupsDAO.delete(5);
+//******************************************
     }
 }
