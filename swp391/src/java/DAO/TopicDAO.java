28c437e45b359f7061f147881a399892ed01ee4f
Anh Tu <tuhase161714@fpt.edu.vn>
2022-06-18 22:58:04 +0700

    topic

--- swp391/src/java/DAO/TopicDAO.java
+++ swp391/src/java/DAO/TopicDAO.java
@@ -5,6 +5,7 @@
  */
 package DAO;
 
+import DTO.Category;
 import DTO.Department;
 import DTO.Topic;
 import DTO.Users;
@@ -96,7 +97,7 @@ public class TopicDAO {
         }
         return list;
     }
-    
+
     public static ArrayList<Topic> filterByDepartment(String name) {
         Connection cn = null;
         ArrayList<Topic> list = new ArrayList<>();
@@ -133,6 +134,128 @@ public class TopicDAO {
             e.getStackTrace();
         }
         return list;
+    }
+
+    public static Category readCategoryByTopicId(int id) {
+        Connection cn = null;
+        Category cate = null;
+        try {
+            cn = DBUtils.makeConnection();
+            if (cn != null) {
+                String sql = "  select c.*\n"
+                        + "  from dbo.Topic t join dbo.Category c on t.CategoryId=c.CategoryId\n"
+                        + "  where t.TopicId=?";
+                PreparedStatement stm = cn.prepareStatement(sql);
+                stm.setInt(1, id);
+                ResultSet rs = stm.executeQuery();
+                while (rs.next()) {
+                    int cateId = rs.getInt("CategoryId");
+                    String cateName = rs.getString("CategoryName");
+                    cate = new Category(cateId, cateName);
+                }
+                cn.close();
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return cate;
+
+    }
+
+    public static Topic readById(int id) {
+        Connection cn = null;
+        Topic topic = null;
+        try {
+            cn = DBUtils.makeConnection();
+            if (cn != null) {
+                String sql = "    select ltt.*,d.Name as DepartmentName,u.Name as LecturerName\n"
+                        + "  from (select t.*,lt.LecturerId from dbo.LecturerTopic lt join dbo.Topic t on lt.TopicId = t.TopicId) ltt \n"
+                        + "  join dbo.Users u on ltt.LecturerId=u.UserId \n"
+                        + "  join dbo.Department d on ltt.DepartmentId=d.DepartmentId\n"
+                        + "  where ltt.TopicId=?";
+                PreparedStatement stm = cn.prepareStatement(sql);
+                stm.setInt(1, id);
+                ResultSet rs = stm.executeQuery();
+                while (rs.next()) {
+                    int topicID = rs.getInt("topicId");
+                    String topicName = rs.getString("Name");
+                    int category = rs.getInt("categoryId");
+                    String description = rs.getString("description");
+                    int businessId = rs.getInt("businessId");
+                    int depId = rs.getInt("DepartmentId");
+                    String depName = rs.getString("DepartmentName");
+                    String lecName = rs.getString("LecturerName");
+                    Users user = new Users();
+                    Department dep = new Department();
+                    dep.setName(depName);
+                    user.setName(lecName);
+                    topic = new Topic(topicID, topicName, category, description, businessId, depId, user, dep);
+
+                }
+                cn.close();
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return topic;
+    }
+
+    public static ArrayList<Topic> findRange(int n1, int n2) {
+        Connection cn = null;
+        ArrayList<Topic> list = new ArrayList<>();
+        try {
+            cn = DBUtils.makeConnection();
+            if (cn != null) {
+                String sql = "  select ltt.*,d.Name as DepartmentName,u.Name as LecturerName\n"
+                        + "  from (select t.*,lt.LecturerId from dbo.LecturerTopic lt join dbo.Topic t on lt.TopicId = t.TopicId) ltt \n"
+                        + "  join dbo.Users u on ltt.LecturerId=u.UserId \n"
+                        + "  join dbo.Department d on ltt.DepartmentId=d.DepartmentId														   \n"
+                        + "  WHERE ltt.TopicId BETWEEN ? and ?";
+                PreparedStatement stm = cn.prepareStatement(sql);
+                stm.setInt(1, n1);
+                stm.setInt(2, n2);
+                ResultSet rs = stm.executeQuery();
+                while (rs.next()) {
+                    int topicID = rs.getInt("topicId");
+                    String name = rs.getString("Name");
+                    int category = rs.getInt("categoryId");
+                    String description = rs.getString("description");
+                    int businessId = rs.getInt("businessId");
+                    int depId = rs.getInt("DepartmentId");
+                    String depName = rs.getString("DepartmentName");
+                    String lecName = rs.getString("LecturerName");
+                    Users user = new Users();
+                    Department dep = new Department();
+                    dep.setName(depName);
+                    user.setName(lecName);
+                    Topic topic = new Topic(topicID, name, category, description, businessId, depId, user, dep);
+                    list.add(topic);
+                }
+                cn.close();
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return list;
+    }
+
+    public static int count() {
+        Connection cn = null;
+        int count = 0;
+        try {
+            cn = DBUtils.makeConnection();
+            if (cn != null) {
+                String sql = "select count(TopicId) as topics from dbo.topic";
+                PreparedStatement stm = cn.prepareStatement(sql);
+                ResultSet rs = stm.executeQuery();
+                while (rs.next()) {
+                    count = rs.getInt("topics");
+                }
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return count;
     }
 
     public static Topic read(Object id) {
