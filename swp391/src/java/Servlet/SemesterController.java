be7c4ca9c6383f82128aa0a7eb123abd86eee2c7
Quang <hongquang7302@gmail.com>
2022-07-09 09:37:52 +0700

    pull ve nhe

--- swp391/src/java/Servlet/SemesterController.java
+++ swp391/src/java/Servlet/SemesterController.java
@@ -4,8 +4,12 @@
  */
 package Servlet;
 
+import DAO.SemesterDAO;
+import DTO.Semester;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -44,9 +48,124 @@ public class SemesterController extends HttpServlet {
         String currAction = (String) session.getAttribute("currSemesterAction");
         switch (action) {
             case "index":
+                SemesterDAO semDao = new SemesterDAO();
+                ArrayList<Semester> list = semDao.readAll();
+                //        pagination
+                if (!prevAction.equals(currAction)) {
+                    session.setAttribute("totalPage", null);
+                    session.setAttribute("page", null);
+                }
+                session.setAttribute("prevSemesterAction", "index");
+                pagination(request, response, list);
+                //        pagination 
+                
                 request.getRequestDispatcher("/manageSemester.jsp").forward(request, response);
                 break;
+            case "search":
+                semDao = new SemesterDAO();
+                String searchText = request.getParameter("searchText");
+                ArrayList<Semester> list1 = null;
+
+                //        pagination
+
+                if (searchText == null) {
+                    list1 = semDao.readAll();
+                } else {
+                    list1 = semDao.readByName(searchText);
+                }
+                if (!prevAction.equals(currAction)) {
+                    session.setAttribute("totalPage", null);
+                    session.setAttribute("page", null);
+                }
+                session.setAttribute("prevSemesterAction", "search");
+                pagination(request, response, list1);
+                session.setAttribute("searchTextSemester", searchText);
+                session.setAttribute("currSemesterAction", "pagesearch");
+                //        pagination
+
+                request.getRequestDispatcher("/manageSemester.jsp").forward(request, response);
+                break;
+            case "pagesearch":
+//                searchText = (String) session.getAttribute("searchTextProject");
+//                proDao = new ProjectDAO();
+//                
+//                if (searchText == null) {
+//                    proList1 = proDao.readAllProject(currSem.getSemesterId());
+//                } else {
+//                    proList1 = proDao.searchByName(searchText, currSem.getSemesterId());
+//                }
+//                pagination(request, response, proList1);
+//                session.setAttribute("prevProjectAction", "pagesearch");
+//
+//                request.getRequestDispatcher("/projectListLecturer.jsp").forward(request, response);
+
+                break;
         }
+    }
+    
+    private void pagination(HttpServletRequest request, HttpServletResponse response, ArrayList<Semester> list) {
+        int pageSize = 5;//Kich thuoc trang                        
+        //Xac dinh so thu tu cua trang hien tai
+
+        HttpSession session = request.getSession();
+        session.setAttribute("totalPage", null);
+        Integer page = (Integer) session.getAttribute("page");
+        if (page == null) {
+            page = 1;
+        }
+
+        //Xac dinh tong so trang
+        Integer totalPage = (Integer) session.getAttribute("totalPage");
+        if (totalPage == null) {
+            int count = list.size();//Dem so luong records
+            totalPage = (int) Math.ceil((double) count / pageSize);//Tinh tong so trang
+        }
+
+        String op = request.getParameter("op");
+        if (op == null) {
+            op = "FirstPage";
+        }
+        switch (op) {
+            case "FirstPage":
+                page = 1;
+                break;
+            case "PreviousPage":
+                if (page > 1) {
+                    page--;
+                }
+                break;
+            case "NextPage":
+                if (page < totalPage) {
+                    page++;
+                }
+                break;
+            case "LastPage":
+                page = totalPage;
+                break;
+            case "GotoPage":
+                page = Integer.parseInt(request.getParameter("gotoPage"));
+                if (page <= 0) {
+                    page = 1;
+                } else if (page > totalPage) {
+                    page = totalPage;
+                }
+                break;
+        }
+
+        //Lay trang du lieu duoc yeu cau
+        List slist;
+        int n1 = (page - 1) * pageSize;
+        int n2 = n1 + pageSize - 1;
+        try {
+            slist = list.subList(n1, n2 + 1);
+        } catch (Exception e) {
+            slist = list.subList(n1, list.size());
+        }//Doc mot trang
+
+        //Luu thong tin vao session va request
+        session.setAttribute("page", page);
+        session.setAttribute("totalPage", totalPage);
+        request.setAttribute("list", slist);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
