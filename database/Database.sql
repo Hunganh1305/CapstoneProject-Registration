d87f6132bf2335bf4c45504d949168acf9623813
Anh Tu <tuhase161714@fpt.edu.vn>
2022-08-01 13:18:05 +0700

    Update Database.sql

--- database/Database.sql
+++ database/Database.sql
@@ -265,7 +265,7 @@ insert into Users values(77,'Giang Oi','12345678',1,1,'giangoi@gmail.com',1,null
 insert into Users values(78,'Khoai Lang Thang','12345678',1,1,'klt@gmail.com',1,null)
 insert into Users values(79,'Ba Tan vlog','12345678',1,1,'tanvl@gmail.com',1,null)
 insert into Users values(80,'Sang Vlog','12345678',1,1,'sangvl@gmail.com',1,null)
-
+insert into Users values(81,'Admin','12345678',1,null,'admin@gmail.com',4,null)
 
 insert into Users values(82,'hongxh','12345678',1,null,'hongxh@fpt.edu.vn',1,null)
 insert into Users values(83,'Quach Tinh','12345678',1,5,'quachtinh@gmail.com',1,null)
@@ -273,7 +273,7 @@ insert into Users values(84,'Ly Binh','12345678',1,5,'lybinh@gmail.com',1,null)
 
 
 --admin
-insert into Users values(81,'Admin','12345678',1,null,'admin@gmail.com',4,null)
+
 
 select * from Users
 
